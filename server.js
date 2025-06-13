7e0c841fd0a0ee6527a39a5d4f8eceac775b613f
The_Pantomath <hetchavadiya@gmail.com>
2025-06-13 05:59:03 +0530

    adding icon

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import subscribeRouter from './src/pages/api/subscribe.js'; 
+
 const app = express();
-// const port = process.env.PORT || 3000;
+
 app.use(cors({
   origin: 'https://www.cocastindia.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
 }));
 
-// Middleware to parse JSON body
 app.use(express.json());
 
-// Root route
 app.get('/', (req, res) => {
   res.send('Welcome to Cocast API server!');
 });
 
-// Import and use your subscribe route (adjust path if needed)
-import subscribeRouter from './src/pages/api/subscribe.js'; 
 app.use('/src/pages/api/subscribe', subscribeRouter);
+
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
